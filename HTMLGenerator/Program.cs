fbbe79347b554b873ff4ffcc257a65b88b48545c
Joakim Olofsson <joakim.olofsson@iths.se>
2024-09-25 16:20:55 +0200

    Adding project files

---
+++ HTMLGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HTMLGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
