a90edee816b687d39e0274f30f51f761d40c5fe9
Joakim Olofsson <joakim.olofsson@iths.se>
2024-09-25 17:03:38 +0200

    Added ToUpper and stuff!

--- HTMLGenerator/Program.cs
+++ HTMLGenerator/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] courses = { " C#", "daTAbaser", "WebbuTVeCkling ", "clean Code " };
+
+            Console.WriteLine(
+                $"<!DOCTYPE html> \n" +
+                $"<html> \n" +
+                $"<body\\> \n" +
+                $"<h1>Välkomna!</h1> \n" +
+                $"<p>Kurs om { char.ToUpper(courses[0].Trim().ToLower()[0]) + courses[0].Trim().ToLower().Substring(1) }</p> \n" +
+                $"<p>Kurs om { char.ToUpper(courses[1].Trim().ToLower()[0]) + courses[1].Trim().ToLower().Substring(1) }</p> \n" +
+                $"<p>Kurs om { char.ToUpper(courses[2].Trim().ToLower()[0]) + courses[2].Trim().ToLower().Substring(1) }</p> \n" +
+                $"<p>Kurs om { char.ToUpper(courses[3].Trim().ToLower()[0]) + courses[3].Trim().ToLower().Substring(1) }</p> \n" +
+                $"</body> \n" +
+                $"</html>"
+            );
         }
     }
 }
