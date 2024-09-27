6e089f7ac1548b9557f3ce0303829e120beb5cd0
Joakim Olofsson <joakim.olofsson@iths.se>
2024-09-27 09:44:00 +0200

    OOP remake is now done

--- HTMLGenerator/Program.cs
+++ HTMLGenerator/Program.cs
@@ -1,59 +1,69 @@
-﻿namespace HTMLGenerator
+﻿using System.Diagnostics;
+
+namespace HTMLGenerator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            /*HtmlGenerator();*/
+            WebsiteGenerator myWebsite = new WebsiteGenerator();
+            myWebsite.HandleInput();
+            myWebsite.GenerateHTML();
         }
 
-        /*static void HtmlGenerator()
+        class WebsiteGenerator
         {
-            Console.WriteLine("Name of the class:");
-            string className = HandleNullAndEmpty(Console.ReadLine());
+            public string className;
+            public List<string> messages = new List<string>();
+            public string[] courses = { " C#", "daTAbaser", "WebbuTVeCkling ", "clean Code " };
 
-            List<string> msgList = new List<string>();
-            int msgAmount = 3;
-
-            for(int i = 0; i < msgAmount; i++)
+            public void HandleInput()
             {
-                Console.WriteLine($"Type message {i + 1} to the class:");
-                msgList.Add($"{HandleNullAndEmpty(Console.ReadLine())}");
-            }            
+                Console.WriteLine("What is the name of the class?");
+                className = HandleNullAndEmpty(Console.ReadLine());
 
-            string[] courses = { " C#", "daTAbaser", "WebbuTVeCkling ", "clean Code " };
-
-            Console.WriteLine(
-                $"\n<!DOCTYPE html> \n" +
-                $"<html> \n" +
-                $"<body> \n" +
-                $"<h1>Välkomna {className}!</h1>"
-            );
-            for (int i = 0; i < msgList.Count; i++)
-            {
-                Console.WriteLine($"<p>Meddelande {i + 1}: {msgList[i]}</p>");
-            };
-            Console.WriteLine(
-                $"<main> \n" +
-                $"<p>Kurs om {char.ToUpper(courses[0].Trim().ToLower()[0]) + courses[0].Trim().ToLower().Substring(1)}</p> \n" +
-                $"<p>Kurs om {char.ToUpper(courses[1].Trim().ToLower()[0]) + courses[1].Trim().ToLower().Substring(1)}</p> \n" +
-                $"<p>Kurs om {char.ToUpper(courses[2].Trim().ToLower()[0]) + courses[2].Trim().ToLower().Substring(1)}</p> \n" +
-                $"<p>Kurs om {char.ToUpper(courses[3].Trim().ToLower()[0]) + courses[3].Trim().ToLower().Substring(1)}</p> \n" +
-                $"</main> \n" +
-                $"</body> \n" +
-                $"</html>"
-            );
-        }*/
-
-        /*static string HandleNullAndEmpty(string? userInput)
-        {
-            while(string.IsNullOrWhiteSpace(userInput))
-            {
-                Console.WriteLine("Invalid input, try something else!");
-                userInput = Console.ReadLine();
+                int msgAmount = 3;
+                for (int i = 0; i < msgAmount; i++)
+                {
+                    Console.WriteLine($"Type message {i + 1} to the class:");
+                    messages.Add($"{HandleNullAndEmpty(Console.ReadLine())}");
+                }
             }
 
-            return userInput;
-        }*/
+            public void GenerateHTML()
+            {
+                Console.WriteLine(
+                    $"\n<!DOCTYPE html> \n" +
+                    $"<html> \n" +
+                    $"<body> \n" +
+                    $"<h1>Välkomna {className}!</h1>"
+                );
+                for (int i = 0; i < messages.Count; i++)
+                {
+                    Console.WriteLine($"<p>Meddelande {i + 1}: {messages[i]}</p>");
+                };
+                Console.WriteLine(
+                    $"<main> \n" +
+                    $"<p>Kurs om {char.ToUpper(courses[0].Trim().ToLower()[0]) + courses[0].Trim().ToLower().Substring(1)}</p> \n" +
+                    $"<p>Kurs om {char.ToUpper(courses[1].Trim().ToLower()[0]) + courses[1].Trim().ToLower().Substring(1)}</p> \n" +
+                    $"<p>Kurs om {char.ToUpper(courses[2].Trim().ToLower()[0]) + courses[2].Trim().ToLower().Substring(1)}</p> \n" +
+                    $"<p>Kurs om {char.ToUpper(courses[3].Trim().ToLower()[0]) + courses[3].Trim().ToLower().Substring(1)}</p> \n" +
+                    $"</main> \n" +
+                    $"</body> \n" +
+                    $"</html>"
+                );
+            }
+
+            static string HandleNullAndEmpty(string? userInput)
+            {
+                while (string.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("Invalid input, try something else!");
+                    userInput = Console.ReadLine();
+                }
+
+                return userInput;
+            }
+        }
     }
 }
